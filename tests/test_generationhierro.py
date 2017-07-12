f0d31aa35ce5d426613b27bc25783ad9c818df72
blackleg <hectorespertpardo@gmail.com>
2017-07-12 16:10:58 +0200

    Add generationhierro test

---
+++ tests/test_generationhierro.py
@@ -0,0 +1,13 @@
+import unittest
+
+from reescrapper import GenerationHierro
+
+
+class TestGenerationHierro(unittest.TestCase):
+
+    def test_instance(self):
+        instance = GenerationHierro()
+        self.assertIsInstance(instance, GenerationHierro)
+
+if __name__ == '__main__':
+    unittest.main()
