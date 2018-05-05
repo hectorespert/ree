74eedd9e1aa5b9593b20038df87a939e50a7d1f2
blackleg <hectorespertpardo@gmail.com>
2018-05-05 19:47:01 +0200

    Improve El Hierro test

--- tests/canary/test_elhierro.py
+++ tests/canary/test_elhierro.py
@@ -14,7 +14,7 @@ class TestElHierro(unittest.TestCase):
         self.adapter = Adapter()
         self.session.mount('https://', self.adapter)
         self.instance = ElHierro(self.session)
-        json_data = resource_string("tests.mocks", "canary.txt").decode("UTF-8")
+        json_data = resource_string("tests.mocks", "el_hierro.txt").decode("UTF-8")
         self.adapter.register_uri(ANY, ANY, text=json_data)
 
     def test_instance(self):
@@ -24,6 +24,11 @@ class TestElHierro(unittest.TestCase):
         response = self.instance.get()
         self.assertIsInstance(response, Response)
         self.assertIsNotNone(response.timestamp)
+        self.assertEqual(response.demand, 3.8)
+        self.assertEqual(response.hydraulic, -3.8)
+        self.assertEqual(response.wind, 7.8)
+        self.assertEqual(response.vapor, 0.0)
+        self.assertEqual(response.diesel, 0.0)
 
     def test_get_all(self):
         responses = self.instance.get_all()
