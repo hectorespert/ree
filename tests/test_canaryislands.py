06c2efdb9088915d12860da891e68363ebfea872
Hector Espert <hectorespertpardo@gmail.com>
2022-06-21 22:28:35 +0200

    Fix La Gomera

--- tests/test_canaryislands.py
+++ tests/test_canaryislands.py
@@ -1,21 +1,12 @@
 import unittest
 
-from pkg_resources import resource_string
-from requests import Session
-from requests_mock import Adapter, ANY
-
 from ree import CanaryIslands, Response
 
 
 class TestCanaryIslands(unittest.TestCase):
 
     def setUp(self):
-        self.session = Session()
-        self.adapter = Adapter()
-        self.session.mount('https://', self.adapter)
-        self.instance = CanaryIslands(self.session)
-        json_data = resource_string("tests.mocks", "canary.txt").decode("UTF-8")
-        self.adapter.register_uri(ANY, ANY, text=json_data)
+        self.instance = CanaryIslands()
 
     def test_instance(self):
         self.assertIsInstance(self.instance, CanaryIslands)
