9b428ce3b4db12d0f00cf60dbb602271f0a5e730
Viktor Andersson <30777521+VIKTORVAV99@users.noreply.github.com>
2022-06-30 10:11:53 +0200

    add typings and date to all `get_all` functions

--- ree/iberianpeninsula.py
+++ ree/iberianpeninsula.py
@@ -1,3 +1,5 @@
+from typing import Union
+
 from .core import Scraper
 
 
@@ -6,5 +8,8 @@ class IberianPeninsula(Scraper):
     def __init__(self, session=None, verify=True):
         super(self.__class__, self).__init__(session, verify)
 
-    def get(self, date=None, last=True):
+    def get(self, date: Union[str, None]=None, last=True):
         return super(self.__class__, self).get("DEMANDAQH", "Europe/Madrid", "Peninsula", date, last)
+
+    def get_all(self, date: Union[str, None]=None):
+        return self.get(date, False)
